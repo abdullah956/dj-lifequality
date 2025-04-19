f1d1fcf5f9ebba6886c45d613d19a8661e9de450
Sohaib-Azhar010 <sohaibazhar04@gmail.com>
2025-04-19 18:53:54 +0500

    added pages , main setup done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view,shop_view,product_view,story_view,about_view,cart_view,contact_view,checkout_view,thanks_view
 
 urlpatterns = [
     path('', home_view, name='hello_world'),
+    path('shop/', shop_view, name='shop'),
+    path('product/', product_view, name='product'),
+    path('story/', story_view, name='story'),
+    path('about/', about_view, name='about'),
+    path('cart/', cart_view, name='cart'),
+    path('contact/', contact_view, name='contact'),
+    path('checkout/', checkout_view, name='checkout'),
+    path('orderplaced/', thanks_view, name='thanks'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
+
+
+def shop_view(request):
+    return render(request, 'shop.html')
+
+def product_view(request):
+    return render(request, 'shop-details.html') 
+
+def story_view(request):
+    return render(request, 'blog.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def cart_view(request):
+    return render(request, 'shopping-cart.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def checkout_view(request):
+    return render(request, 'checkout.html')
+
+def thanks_view(request):
+    return render(request, 'thanks.html')
