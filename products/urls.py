14cd7197c5f0e97bc988fe70e99a68b743c81b12
abdullah956 <abdullaharshed956@gmail.com>
2025-04-28 18:14:20 +0500

    cart sort

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import products_view,product_detail_view,category_products_view
+from .views import products_view,product_detail_view,category_products_view,products_by_category
 urlpatterns = [
     path('', products_view, name='products'),
     path('product/<int:id>/', product_detail_view, name='product_detail'),
     path('category/<int:category_id>/', category_products_view, name='category_products'),
-    
+    path('products/<int:category_id>/', products_by_category, name='products_by_category')
+
 ]
--- products/views.py
+++ products/views.py
@@ -22,3 +22,15 @@ def category_products_view(request, category_id):
     products = Product.objects.filter(category=category)
     categories = Category.objects.all()
     return render(request, 'products/products.html', {'products': products, 'categories': categories, 'selected_category': category})
+
+def products_by_category(request, category_id):
+    sort = request.GET.get('sort')
+    category = Category.objects.get(id=category_id)
+    products = Product.objects.filter(category=category)
+    
+    if sort == 'low':
+        products = products.order_by('price')
+    elif sort == 'high':
+        products = products.order_by('-price')
+        
+    return render(request, 'products/products.html', {'category': category, 'products': products})
